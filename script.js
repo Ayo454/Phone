f7bf8fe00cc04f3bee9f6fc67a379ac46dcb7e96
Ayo454 <ayomideoluniyi49@gmail.com>
2025-11-25 02:23:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -511,10 +511,12 @@ if (mobileMenuBtn) {
 // Add scroll-based navbar background
 window.addEventListener('scroll', () => {
     const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
+    if (navbar) {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
     }
 });
 
